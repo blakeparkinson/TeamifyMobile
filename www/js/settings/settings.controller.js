6c250e3a9f7ec6489a17b7b28ca0d4ff0186b982
sodapopinsky <Nick@Nicholass-iMac.local>
2016-03-22 17:18:15 -0500

    f

--- www/js/settings/settings.controller.js
+++ www/js/settings/settings.controller.js
@@ -4,8 +4,32 @@
     'use strict';
     var module = angular.module('app.settings');
 
-    module.controller('SettingsController', function SettingsController($log) {
-        $log.log('here');
+    module.controller('SettingsController', function SettingsController($log, deployChannel, $scope) {
+        var deploy = new Ionic.Deploy();
+        deploy.setChannel(deployChannel);
+
+        // Update app code with new release from Ionic Deploy
+        $scope.doUpdate = function() {
+            deploy.update().then(function(res) {
+                console.log('Ionic Deploy: Update Success! ', res);
+            }, function(err) {
+                console.log('Ionic Deploy: Update error! ', err);
+            }, function(prog) {
+                console.log('Ionic Deploy: Progress... ', prog);
+            });
+        };
+
+        // Check Ionic Deploy for new code
+        $scope.checkForUpdates = function() {
+            console.log('Ionic Deploy: Checking for updates');
+            deploy.check().then(function(hasUpdate) {
+                console.log('Ionic Deploy: Update available: ' + hasUpdate);
+                $scope.hasUpdate = hasUpdate;
+            }, function(err) {
+                console.error('Ionic Deploy: Unable to check for updates', err);
+            });
+        }
+
     });
 
 
