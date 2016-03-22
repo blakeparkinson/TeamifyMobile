b72de7db61c8e798c68035f7f89052f1e8f0b82e
sodapopinsky <Nick@Nicholass-iMac.local>
2016-03-22 17:37:07 -0500

    f

--- www/main.js
+++ www/main.js
@@ -507,57 +507,6 @@ core.filter('initials', function () {
 });
 
 //
-angular.module('app.settings')
-    .config(function($stateProvider) {
-        $stateProvider
-            .state('app.settings', {
-
-                url: '/settings',
-                views: {
-                    'menuContent': {
-                        templateUrl: 'js/settings/settings.html'
-                    }
-                }
-            });
-
-    });
-
-(function () {
-
-    'use strict';
-    var module = angular.module('app.settings');
-
-    module.controller('SettingsController', function SettingsController($log, deployChannel, $scope) {
-        var deploy = new Ionic.Deploy();
-        deploy.setChannel(deployChannel);
-
-        // Update app code with new release from Ionic Deploy
-        $scope.doUpdate = function() {
-            deploy.update().then(function(res) {
-                console.log('Ionic Deploy: Update Success! ', res);
-            }, function(err) {
-                console.log('Ionic Deploy: Update error! ', err);
-            }, function(prog) {
-                console.log('Ionic Deploy: Progress... ', prog);
-            });
-        };
-
-        // Check Ionic Deploy for new code
-        $scope.checkForUpdates = function() {
-            console.log('Ionic Deploy: Checking for updates');
-            deploy.check().then(function(hasUpdate) {
-                console.log('Ionic Deploy: Update available: ' + hasUpdate);
-                $scope.hasUpdate = hasUpdate;
-            }, function(err) {
-                console.error('Ionic Deploy: Unable to check for updates', err);
-            });
-        }
-
-    });
-
-
-})();
-
 
 angular.module('app.messages')
     .config(function($stateProvider) {
@@ -674,6 +623,57 @@ angular.module('app.selectOrganization')
     });
 
 })();
+angular.module('app.settings')
+    .config(function($stateProvider) {
+        $stateProvider
+            .state('app.settings', {
+
+                url: '/settings',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'js/settings/settings.html'
+                    }
+                }
+            });
+
+    });
+
+(function () {
+
+    'use strict';
+    var module = angular.module('app.settings');
+
+    module.controller('SettingsController', function SettingsController($log, deployChannel, $scope) {
+        var deploy = new Ionic.Deploy();
+        deploy.setChannel(deployChannel);
+
+        // Update app code with new release from Ionic Deploy
+        $scope.doUpdate = function() {
+            deploy.update().then(function(res) {
+                console.log('Ionic Deploy: Update Success! ', res);
+            }, function(err) {
+                console.log('Ionic Deploy: Update error! ', err);
+            }, function(prog) {
+                console.log('Ionic Deploy: Progress... ', prog);
+            });
+        };
+
+        // Check Ionic Deploy for new code
+        $scope.checkForUpdates = function() {
+            console.log('Ionic Deploy: Checking for updates');
+            deploy.check().then(function(hasUpdate) {
+                console.log('Ionic Deploy: Update available: ' + hasUpdate);
+                $scope.hasUpdate = hasUpdate;
+            }, function(err) {
+                console.error('Ionic Deploy: Unable to check for updates', err);
+            });
+        }
+
+    });
+
+
+})();
+
 (function () {
     'use strict';
 
